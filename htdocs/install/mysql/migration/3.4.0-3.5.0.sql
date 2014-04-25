ddbb1d9b1489a436e049e1b257c89f877761777f
jfefe <jfefe@aternatik.fr>
2014-04-25 16:32:56 +0200

    Work on adding catgory translation
    Conflicts:
    	htdocs/install/mysql/migration/3.5.0-3.6.0.sql

--- htdocs/install/mysql/migration/3.4.0-3.5.0.sql
+++ htdocs/install/mysql/migration/3.4.0-3.5.0.sql
@@ -368,3 +368,28 @@ ALTER TABLE llx_adherent ADD skype VARCHAR(255) AFTER email;
 -- multi-rib
 ALTER TABLE llx_societe_rib ADD default_rib smallint NOT NULL DEFAULT 0 AFTER owner_address;
 UPDATE llx_societe_rib SET default_rib = 1;
+
+-- New : extrafield on categories
+create table llx_categories_extrafields
+(
+  rowid                     integer AUTO_INCREMENT PRIMARY KEY,
+  tms                       timestamp,
+  fk_object                 integer NOT NULL,
+  import_key                varchar(14)                          		-- import key
+) ENGINE=innodb;
+
+ALTER TABLE llx_categories_extrafields ADD INDEX idx_categories_extrafields (fk_object);
+
+
+-- New : category translation
+create table llx_categorie_lang
+(
+  rowid          integer AUTO_INCREMENT PRIMARY KEY,
+  fk_category    integer      DEFAULT 0 NOT NULL,
+  lang           varchar(5)   DEFAULT 0 NOT NULL,
+  label          varchar(255) NOT NULL,
+  description    text
+)ENGINE=innodb;
+
+ALTER TABLE llx_categorie_lang ADD UNIQUE INDEX uk_category_lang (fk_category, lang);
+ALTER TABLE llx_categorie_lang ADD CONSTRAINT fk_category_lang_fk_category 	FOREIGN KEY (fk_category) REFERENCES llx_categorie (rowid);
